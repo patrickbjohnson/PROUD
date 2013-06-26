7cf7e9bad3d02904e406c552788a2b31455a1855
Luqmaan <ldawoodjee@gmail.com>
2013-06-26 18:33:39 -0400

    Fancy scroll, broken resizing

--- code/js/main.js
+++ code/js/main.js
@@ -1,12 +1,82 @@
-$(document).ready(function(){
-	$('a[href^="#"]').on("click", function (e) {
-		e.preventDefault();
+function initNavScroll($links) {
+	$links.on("click", function(e) {
 		var target = this.hash,
-		$target = $(target);
+			$target = $(target);
+
 		$('html, body').stop().animate({
 			'scrollTop': $target.offset().top
-		}, 1500, 'swing', function (){
+		}, 1500, 'swing', function() {
 			window.location.hash = target;
 		});
+
+		e.preventDefault();
 	});
+}
+
+var zoomer = {
+	scale: 1.0,
+	wh: 0,
+	ww: 0,
+	$window: $(window),
+	$background: $(".bg-wrapper img"),
+	init: function() {
+
+		var self = this;
+
+		this.updateHeights();
+
+		this.$window.on("resize", function() {
+			self.updateHeights.call(self);
+		});
+		this.$window.on("scroll", function() {
+			self.zoomEffect.call(self);
+		})
+
+	},
+	zoomEffect: function() {
+
+		var t = this.$window.scrollTop();
+
+		if (t < 0) {
+			this.scale = (1.0 - ( t / this.wh ) ) ;
+			this.$background.css({
+				"-webkit-transform": "translate3d(0px, 0px, 0px) scale(" + this.scale + ")"
+			});
+		}
+		else if (t + this.wh >= this.dh) {
+			this.scale = 1.0 - (this.t / this.wh);
+			console.log("scale")
+			this.$background.css({
+				"-webkit-transform": "translate3d(0px, 0px, 0px) scale(" + this.scale + ")"
+			});
+		}
+		else {
+			this.$background.css({
+				"-webkit-transform": "translate3d(0px, 0px, 0px) scale(1.0)"
+			});
+		}
+
+	},
+	updateHeights: function() {
+		this.wh = $(window).height();
+		this.dh = $(document).height();
+	}
+};
+
+function initScrollZoom() {
+
+	var $background = $(".bg-wrapper img");
+	var scale = 1.0;
+	var $window = $(window);
+	var $wh = 0, dh = 0;
+
+
+}
+
+
+$(document).ready(function() {
+
+	initNavScroll($(".nav-primary a"));
+	zoomer.init();
+
 });
